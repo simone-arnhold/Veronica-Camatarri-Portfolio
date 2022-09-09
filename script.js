f30e797654f268c3c0e13faaec2fc96d44f3900a
simone-arnhold <110480573+simone-arnhold@users.noreply.github.com>
2022-09-09 20:07:40 +0200

    add Banners

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(window).on("load", function () {
+  // mobile nav logic
+  const mobileNav = document.querySelector(".hamburger")
+  const navMenu = document.querySelector(".mobile-nav")
+  const mobileNavBtn = mobileNav.children[0]
+  console.log(mobileNav.children[0])
+  mobileNav.addEventListener("click", () => {
+    navMenu.classList.toggle("open")
+    if (mobileNavBtn.classList.contains("fa-bars")) {
+      mobileNavBtn.classList.remove("fa-bars")
+      mobileNavBtn.classList.add("fa-x")
+    } else {
+      mobileNavBtn.classList.remove("fa-x")
+      mobileNavBtn.classList.add("fa-bars")
+    }
+  })
+
   const folder = "/other_images/"
   let imagesArray = []
 

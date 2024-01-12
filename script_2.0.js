51696373a78faef111ea2d9c39249ee07e114719
Simone Arnhold <simone.arnhold@outlook.com>
2024-01-12 12:36:39 +0100

    move files for v2

---
+++ script_2.0.js
@@ -0,0 +1,22 @@
+let text1 = document.getElementById("menu-item-1");
+let text2 = document.getElementById("menu-item-2");
+let text3 = document.getElementById("menu-item-3");
+let text4 = document.getElementById("menu-item-4");
+
+
+function typeWriter(element, text, speed) {
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+typeWriter(text1, " Curriculum //", 50);
+typeWriter(text2, " Graphic Design //", 50);
+typeWriter(text3, " About //", 50);
+typeWriter(text4, " Contact //", 50);
